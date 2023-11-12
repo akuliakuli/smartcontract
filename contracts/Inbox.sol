8a47992540ae02eca3b5109418767968e9b9fb39
akuliakuli <kabby889@gmail.com>
2023-11-12 14:12:44 +0400

    okay

---
+++ contracts/Inbox.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.23;
+contract Inbox {
+    string public message;
+    
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
